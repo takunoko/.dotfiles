7fa068395945cbee64dfe4d6b0404ded14e921c4
takunoko <takunoko@gmail.com>
2020-10-10 09:47:14 +0000

    setup_zshだけでzshpreztoの初期化まで完了するように修正

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,34 +1,26 @@
-#!/bin/sh
+#!/bin/sh -x
+# #!/bin/sh -x  # 実行コマンドを出力しながら
 
-# 過去のファイルを削除
-rm -rf "${ZDOTDIR:-$HOME}/.zprezto"
-rm -rf "${ZDOTDIR:-$HOME}/zsh_orig"
+# zshがインストールされているか?
+if !(type "zsh" > /dev/null 2>&1); then
+  echo "zsh is not installed."
+  echo "Please install zsh before run this script."
+  return -1
+fi
 
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-mkdir zsh_orig && mv zshmv .zlogin .zlogout .zprofile .zshenv  zsh_orig
- 
-# add zprezto original ".zshrc"
-echo "source '${ZDOTDIR:-$HOME}/.zprezto/init.zsh'" >> $HOME/.zshrc
-echo "source ~/.dotfiles/zsh/.zshrc.main" >> $HOME/.zshrc
+# zshpreztoの初期化
+while read line
+do
+  echo "$line" | zsh
+done < 'setup_zshprezto.cmd'
 
-# Shellのデフォルトに設定
-# chsh -s /bin/zsh
+# # Shellのデフォルトに設定
+# echo "chsh -s `which zsh` を実行します。パスワードを入力"
+# chsh -s `which zsh`
 
-# gitのaliasを登録。ここにかくべきではない...?
-git config --global alias.co checkout
-git config --global alias.st status
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.lt log --graph --pretty=oneline --abbrev-commit
-
-# zshでgitコマンドをつかうときに自動で^をエスケープしてくれる
-curl https://raw.githubusercontent.com/knu/zsh-git-escape-magic/master/git-escape-magic > ${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions/git-escape-magic
-
-# zsh環境じゃなきゃ実行できない。。(/bin/shじゃ駄目)
-# 手動で実行しよう。
-echo '
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-'
-echo "setopt EXTENDED_GLOB\nを実行してください"
+# # gitのaliasを登録。ここにかくべきではない...?
+# git config --global alias.co checkout
+# git config --global alias.st status
+# git config --global alias.br branch
+# git config --global alias.ci commit
+# git config --global alias.lt log --graph --pretty=oneline --abbrev-commit
