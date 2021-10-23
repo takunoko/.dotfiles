c7289c5acdc6ef25b24f6bb165f0c19f03cee59d
takunoko <takunoko@gmail.com>
2021-10-23 11:20:49 +0900

    謎ファイルを消してインストールコマンドを作った

---
+++ install_command.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+# 作成中。。。
+
+# install pyenv
+brew install zsh htop jq rg git nvim tree exa lsd
+
+# install pyenv
+brew install pyenv
+
+# install rbenv
+brew install rbenv
+
+brew install ruby-build
+brew install rbenv-gemset
+brew install rbenv-gem-rehash
